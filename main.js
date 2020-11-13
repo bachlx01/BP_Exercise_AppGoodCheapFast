7ecfce49a5ee55b43f12a21809276a2918805a5f
bachlx01 <73922823+bachlx01@users.noreply.github.com>
2020-11-13 16:31:19 +0700

    smart logic

--- main.js
+++ main.js
@@ -1,31 +1,6 @@
-// document.getElementById('good').checked = true;
-
-    let good = document.getElementById('good');
-    let cheap = document.getElementById('cheap');
-    let fast = document.getElementById('fast');
-
-function checking1 () {
-    if (good.checked == true) {
-        document.getElementById('cheap').checked = false;
+function checking(e) {
+    let arrayId = ['good', 'cheap', 'fast', 'good', 'cheap', 'fast', 'cheap', 'fast'];
+    if (e.checked == true && document.getElementById(arrayId[arrayId.indexOf(e.getAttribute('id')) + 2]).checked == true) {
+        document.getElementById(arrayId[arrayId.indexOf(e.getAttribute('id')) + 1]).checked = false;
     }
 }
-
-function checking2 () {
-    if (cheap.checked == true) {
-        document.getElementById('fast').checked = false;
-    }
-}
-function checking3 () {
-    if (fast.checked == true) {
-        document.getElementById('good').checked = false;
-    }
-}
-
-
-    // if (good.checked == true && cheap.checked == true) {
-    //     document.getElementById('fast').checked = false;
-    // } else if (good.checked == true && fast.checked == true) {
-    //     document.getElementById('cheap').checked = false;
-    // } else if (cheap.checked == true && fast.checked == true) {
-    //     document.getElementById('good').checked = false;
-    // }
